6eb2cda8fa7a0202aec60c944d8e2fcf6fea314d
ev0x <ev0x@phrack.com.au>
2014-06-03 18:11:27 +1000

    Added regex for list item non isdigit and isalpha byte level matching only for isdigit and isalpha

--- genregex.py
+++ genregex.py
@@ -7,13 +7,46 @@ for line in sys.stdin:
     co = 0
     theList = []
     for i in l:
-	if i.isalpha:
-            iLen = len(i)
+        iLen = len(i)
+	if i.isalpha():
 	    if co is 0:
 	        theList.append('^[a-zA-Z]{' + str(iLen) + '}')
-            elif co is lLen:
-                theList.append('[a-zA-Z]{' + str(iLen) + '}')
+            elif co is lLen - 1:
+                theList.append('[a-zA-Z]{' + str(iLen) + '}$')
             else:
                 theList.append('[a-sA-Z]{' + str(iLen) + '}')
             co += 1
+        elif i.isdigit():
+            if co is 0:
+                theList.append('^[0-9]{' + str(iLen) + '}')
+            elif co is lLen - 1:
+                theList.append('[0-9]{' + str(iLen) + '}$')
+            else:
+                theList.append('[0-9]{' + str(iLen) + '}')
+            co += 1
+        else:
+            innerList = []
+	    innerco = 0
+            innerlLen = len(i)
+            for s in i:
+		if s.isalpha():
+                    if innerco is 0 and co is 0: 
+                        innerList.append('^[a-zA-Z]')
+                    elif innerco is innerlLen - 1 and co is lLen - 1:
+                        innerList.append('[a-zA-Z]$')
+                    else:
+                        innerList.append('[a-zA-Z]')
+	        elif s.isdigit():
+                    if innerco is 0 and co is 0:
+                        innerList.append('^[0-9]')
+                    elif innerco is innerlLen - 1 and co is lLen - 1:
+                        innerList.append('[0-9]$')
+                    else:
+                        innerList.append('[0-9]')
+                innerco += 1
+            co += 1
+
+            theList.append(''.join(innerList))
+
     print '\s'.join(theList)
+    print line
